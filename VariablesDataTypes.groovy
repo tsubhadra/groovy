b723cf9020f92e220021f20e75c586ff07d9875c
tsubhadra <tsubhadra@gmail.com>
2025-08-29 12:40:49 -0700

    Create VariablesDataTypes.groovy

---
+++ VariablesDataTypes.groovy
@@ -0,0 +1,10 @@
+def name = "Arun"    // dynamic
+String lang = "Groovy"  // explicit type
+
+def age = 25
+def list = [1, 2, 3, 4]
+def map = [id:101, course:"Groovy Basics"]
+
+println "Hello $name, learning $lang at age $age"
+println "List: $list"
+println "Map: $map"
